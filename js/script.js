8925d1d398fc902c1602a2abce53236acadcf1d8
John Torres <john.torres.hurtado@gmail.com>
2017-08-23 08:41:10 -0700

    initial commit
    added index and variable pages

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+function dragon() {
+  var cardInfo = ['Account type','Payment type','Tax documents'];
+
+  var cardBody = ['Pay as you go','Credit Card','221-222-333-44'
+  ];
+  var cardSecondary = ['Renews end of day June 22','Visa ending 4204 · Exp 12/19','Last updated Oct 2016' ];
+  var cards = cardInfo.length;
+  var index = 0;
+
+  // function
+  while (cards> 0){
+    $('.deck').append(
+      "<div class='card'><div class='card-title'>"+cardInfo[index]+"</div>" +
+        "<div class='card-body'><h4>"+ cardBody[index]+"<h4>\
+        <p>"+cardSecondary[index]+"</p>\
+        </div>\
+      </div>"
+    );
+    cards--;
+    index++;
+  }
+}
+
+
+dragon();
+
+
+// $(document).ready(function() {
+//   var cards = 3;
+//   while (cards > 0){
+//     $('.cards').append("<b>Appended text</b>");
+//     cards --;
+//   }
+// });
