1fdb49e209ef6b4ae407e7cf6eed9805db39680d
John Torres <john.torres.hurtado@gmail.com>
2017-09-13 11:45:20 -0700

    made chart fully functional

--- js/script.js
+++ js/script.js
@@ -1,27 +1,48 @@
-
+const cardData ={
+  cardInfo : ['Account type','Payment type','Tax documents'],
+  cardBody :['Pay as you go','Credit Card','221-222-333-44'],
+  cardSecondary : ['Renews end of day June 22','Visa ending 4204 · Exp 12/19','Last updated Oct 2016'],
+}
 function dragon() {
-  var cardInfo = ['Account type','Payment type','Tax documents'];
-
-  var cardBody = ['Pay as you go','Credit Card','221-222-333-44'
-  ];
-  var cardSecondary = ['Renews end of day June 22','Visa ending 4204 · Exp 12/19','Last updated Oct 2016' ];
-  var cards = cardInfo.length;
+  var cards = cardData.cardInfo.length;
   var index = 0;
-
   // function
   while (cards> 0){
     $('.deck').append(
-      "<div class='card'><div class='card-title'><h4>"+cardInfo[index]+"</h4></div>" +
-        "<div class='card-body'><h4>"+ cardBody[index]+"<h4>\
-        <p>"+cardSecondary[index]+"</p>\
-        </div>\
-      </div>"
+      `<div class='card'>
+        <div class='card-title'>
+          <h4>${cardData.cardInfo[index]}</h4>
+        </div>
+        <div class='card-body'>
+          <h4>${cardData.cardBody[index]}</h4>
+          <p>${cardData.cardSecondary[index]}</p>
+        </div>
+      </div>`
     );
     cards--;
     index++;
   }
 }
 
+
+
 $(document).ready(function() {
   dragon();
 });
+
+
+$('.x-values p').mouseenter(function(){
+  $(this).append("<kbd class='tooltip-graph'>$3780.00</kbd>");
+})
+
+$('.x-values p').mouseleave(function(){
+  $(this).find('kbd').remove();
+})
+
+$('.x-values p').click(function(){
+  $('.click').remove();
+  $(this).append("<span class='tooltip-graph click'>$3780.00</span>");
+})
+$('.trend').mouseleave(function(){
+  $('.click').remove();
+})
