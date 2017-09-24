ba85d7f8fcd4788779280daaf58c1d9ede4d1402
John Torres <john.torres.hurtado@gmail.com>
2017-09-23 22:15:00 -0700

    adding working popup

--- js/script.js
+++ js/script.js
@@ -4,6 +4,34 @@ const cardData ={
   cardSecondary : ['Renews end of day June 22','Visa ending 4204 · Exp 12/19','Last updated Oct 2016'],
 }
 
+function popAnimate(){
+  $('.overlay').addClass('show');
+  setTimeout(function(){
+    $('.show').css('animation-play-state','paused');
+  },600);
+
+
+  setTimeout(function(){
+    $('.popup').addClass('appear');
+
+    setTimeout(function(){
+      $('.appear').css('animation-play-state','paused');
+    },600);
+  },200);
+
+  $('.popup-header, .save').click(function(){
+      $('.appear').css('animation-play-state','running');
+      setTimeout(function(){
+        $('.overlay').css('animation-play-state','running');
+      },200);
+
+      setTimeout(function(){
+        $('.overlay').remove();
+      },1000);
+    }
+  );
+}
+
 function dragon() {
   var cards = cardData.cardInfo.length;
   var index = 0;
@@ -37,9 +65,30 @@ function dragon() {
   }
 }
 
+function chngPswd(){
+  $('body').append(
+    `<div class="overlay" style='opacity:0'>
+      <div class="popup" style='opacity:0'>
+        <div class="popup-header">Change Password</div>
+        <div class="popup-body">
+          <label for="other">Previous Password</label>
+          <input type="password" id='old-pswd'>
+          <label for="other">New Password</label>
+          <input type="password" id='new-pswd'>
+          <label for="other">Retype new password</label>
+          <input type="password" id='retype'>
+        </div>
+        <div class="popup-footer">
+          <!-- <button class="secondary">Cancel</button> -->
+          <button class="primary save">Save</button>
+        </div>
+      </div>
+    </div>`
+  );
+  popAnimate();
+}
+
+
 $(document).ready(function() {
-  // dragon();
-  $('.chng-pswd').click(function(){
-    $(this).css('color','red');
-  });
+  $('.chng-pswd').click(chngPswd);
 });
