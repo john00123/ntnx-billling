0c58918f5751e82c2d24a3b9d0a24d0cc81d1479
John Torres <john.torres.hurtado@gmail.com>
2017-09-24 13:13:59 -0700

    made changes on the js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,45 @@
-const cardData ={
-  cardInfo : ['Profile','Account','Authentication & Users'],
-  cardBody :['Pay as you go','Credit Card','221-222-333-44'],
-  cardSecondary : ['Renews end of day June 22','Visa ending 4204 · Exp 12/19','Last updated Oct 2016'],
+const popupData ={
+  title :[
+    'Multifactor Authentication',
+    'Account Information',
+    'Profile Information',
+    'Change Password'],
+
+  body: [
+    `<h1>Improve your account security with a 2 step verification process</h1>
+    <p>Each time you sign in you'll need your password and a verification code sent directly to a phone.</p>`,
+
+    `<label for="other">Company Name</label>
+    <input type="text" value='Nutanix Corporate' id='old-pswd'>
+    <label for="other">Address</label>
+    <input type="Address" value='1740 Technology Drive' id='new-pswd' style='margin-bottom:10px'>
+    <input type="Address" value='San Jose, California, 95110, United States' id='new-pswd2' >
+    <label for="other">Phone Number</label>
+    <input type="Phone" value='+ 1-(408)-000-0000' id='retype'>`,
+
+    `<label for="other">Name</label>
+    <input type="text" value='Lipa Dua' id='old-pswd'>
+    <label for="other">Email</label>
+    <input type="email" value='dualipa@nutanix.com' id='new-pswd'>
+    <label for="other">Phone Number</label>
+    <input type="Phone" value='+ 1-(408)-000-0000' id='retype'>`,
+
+    `<label for="other">Previous Password</label>
+    <input type="password" id='old-pswd'>
+    <label for="other">New Password</label>
+    <input type="password" id='new-pswd'>
+    <label for="other">Retype new password</label>
+    <input type="password" id='retype'>`
+  ],
+
+  footer:[
+    `<button class="secondary">Cancel</button>
+     <button class="primary save">Enable</button>`,
+    `<button class="primary save">Save</button>`,
+    `<button class="primary save">Save</button>`,
+    `<button class="primary save">Save</button>`]
 }
+
 
 function popAnimate(){
   $('.overlay').addClass('show');
@@ -23,135 +60,22 @@ function popAnimate(){
   );
 }
 
-function dragon() {
-  var cards = cardData.cardInfo.length;
-  var index = 0;
-  // function
-  while (cards> 0){
-    $('.deck').append(
-      `<div class='card'>
-        <div class='card-title'>
-          <h4>${cardData.cardInfo[index]}</h4>
-        </div>
-        <div class='card-body'>
-          <h4>${cardData.cardBody[index]}</h4>
-          <p>${cardData.cardSecondary[index]}</p>
-        </div>
-      </div>`);
-
-      if(cards === 3){
-        $('.deck').append(
-          `<div class='card'>
-            <div class='card-title'>
-              <h4>${cardData.cardInfo[index]}</h4>
-            </div>
-            <div class='card-body'>
-              <h4>${cardData.cardBody[index]}</h4>
-              <h4>${cardData.cardSecondary[index]}</h4>
-            </div>
-          </div>`
-      )};
-    cards--;
-    index++;
-  }
-}
-
-function chngPswd(){
+function popupContent(i){
   $('body').append(
     `<div class="overlay" style='opacity:0'>
       <div class="popup" style='opacity:0'>
-        <div class="popup-header">Change Password</div>
-        <div class="popup-body">
-          <label for="other">Previous Password</label>
-          <input type="password" id='old-pswd'>
-          <label for="other">New Password</label>
-          <input type="password" id='new-pswd'>
-          <label for="other">Retype new password</label>
-          <input type="password" id='retype'>
-        </div>
-        <div class="popup-footer">
-          <!-- <button class="secondary">Cancel</button> -->
-          <button class="primary save">Save</button>
-        </div>
+        <div class="popup-header">${popupData.title[i]}</div>
+        <div class="popup-body">${popupData.body[i]}</div>
+        <div class="popup-footer">${popupData.footer[i]}</div>
       </div>
     </div>`
   );
   popAnimate();
 }
-
-
-function profile(){
-  $('body').append(
-    `<div class="overlay" style='opacity:0'>
-      <div class="popup" style='opacity:0'>
-        <div class="popup-header">Profile Information</div>
-        <div class="popup-body">
-          <label for="other">Name</label>
-          <input type="text" value='Lipa Dua' id='old-pswd'>
-          <label for="other">Email</label>
-          <input type="email" value='dualipa@nutanix.com' id='new-pswd'>
-          <label for="other">Phone Number</label>
-          <input type="Phone" value='+ 1-(408)-000-0000' id='retype'>
-        </div>
-        <div class="popup-footer">
-          <!-- <button class="secondary">Cancel</button> -->
-          <button class="primary save">Save</button>
-        </div>
-      </div>
-    </div>`
-  );
-  popAnimate();
-}
-
-
-function account(){
-  $('body').append(
-    `<div class="overlay" style='opacity:0'>
-      <div class="popup" style='opacity:0'>
-        <div class="popup-header">Account Information</div>
-        <div class="popup-body">
-          <label for="other">Company Name</label>
-          <input type="text" value='Nutanix Corporate' id='old-pswd'>
-          <label for="other">Address</label>
-          <input type="Address" value='1740 Technology Drive' id='new-pswd' style='margin-bottom:10px'>
-          <input type="Address" value='San Jose, California, 95110, United States' id='new-pswd2' >
-          <label for="other">Phone Number</label>
-          <input type="Phone" value='+ 1-(408)-000-0000' id='retype'>
-        </div>
-        <div class="popup-footer">
-          <!-- <button class="secondary">Cancel</button> -->
-          <button class="primary save">Save</button>
-        </div>
-      </div>
-    </div>`
-  );
-  popAnimate();
-}
-
-function multifactor(){
-  $('body').append(
-    `<div class="overlay" style='opacity:0'>
-      <div class="popup" style='opacity:0'>
-        <div class="popup-header">Enable Multifactor Authentication</div>
-        <div class="popup-body">
-          <h1>Improve your account security with Multifactor Authentication</h1>
-          <p>Each time you sign in you'll need your password and a verification code sent directly to a phone.</p>
-        </div>
-        <div class="popup-footer">
-          <button class="secondary">Cancel</button>
-          <button class="primary save">Enable</button>
-        </div>
-      </div>
-    </div>`
-  );
-  popAnimate();
-}
-
-
 
 $(document).ready(function() {
-  $('.chng-pswd').click(chngPswd);
-  $('.profile .card-title a').click(profile);
-  $('.account .card-title a').click(account);
-  $('.enable-mult-auth').click(multifactor);
+  $('.chng-pswd').click(function(){popupContent(3)});
+  $('.profile .card-title a').click(function(){popupContent(2)});
+  $('.account .card-title a').click(function(){popupContent(1)});
+  $('.enable-mult-auth').click(function(){popupContent(0)});
 });
